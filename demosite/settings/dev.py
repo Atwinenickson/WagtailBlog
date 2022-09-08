3f9521c44bda8386d705049573242d0d2391fe5a
Atwine Nickson <atwine.nickson@sybyl.com>
2022-09-08 13:10:04 +0300

    create a new project blog for wagtail

---
+++ demosite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-=op^$3^giqinbto6^6j7w*w&pjw%!ds1$y+f-8*_-xs)*yjqzi"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
