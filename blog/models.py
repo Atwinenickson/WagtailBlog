d00f306562de41ab4f8c4540ae96e3e076f25bc4
Atwine Nickson <atwine.nickson@sybyl.com>
2022-09-08 14:03:39 +0300

    streamfieldblocks

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from wagtail.core.models import Page
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.core.fields import RichTextField
 
 # Create your models here.
+
+class BlogListingPage(Page):
+    background_image = models.ForeignKey(
+        'wagtailimages.Image',
+        blank=False,
+        null=True,
+        related_name='+',
+        help_text='Header background image',
+        on_delete=models.SET_NULL,
+    )
+
+    headline_text = models.CharField(
+        max_length=70,
+        blank=True, 
+        help_text='Blog listing page header text.'
+    )
+
+    content_panels = Page.content_panels + [
+        ImageChooserPanel("background_image"), 
+        FieldPanel("headline_text"),
+    ]
+
+    subpage_types = ["blog.BlogPage", ]
+
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        blogpages = self.get_children().live().order_by('-first_published_at')
+        context['blogpages'] = blogpages
+        return context
+
+class BlogPage(Page):
+    date = models.DateField("Article Date")
+    intro = models.TextField("Introduction")
+    body = RichTextField(
+        blank=True,
+    )
+
+    featured = models.BooleanField(default=False)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('featured'),
+        FieldPanel('intro'),
+        FieldPanel('body', classname="full"),
+    ]
--- home/models.py
+++ home/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 from wagtail.models import Page
+from blog.models import BlogPage
 
 
 class HomePage(Page):
-    pass
+    
+    def get_context(self, request):
+        context = super().get_context(request)
+        featured_articles = BlogPage.objects.live().filter(featured=True)
+        context['featured_articles'] = featured_articles
+        return context
